be1717496d393dcbb75625d2003f44975409c0fd
Matheus Holanda <matheus.albuquerque@agilecontent.com>
2020-04-13 15:09:25 -0300

    Initial commit.

---
+++ AnimatedAlertView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimatedAlertView
+//
+//  Created by Matheus Holanda Pinheiro de Albuquerque on 13/04/20.
+//  Copyright © 2020 Matheus Holanda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
